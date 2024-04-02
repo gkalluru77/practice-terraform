6fef99e518d74fe6805e4edf6c4a1095a111c822
girin <girinath.kalluru@gmail.com>
2024-04-02 14:01:34 +0530

    Ansible project setu using role concept

--- 02-variables/variable.tf
+++ 02-variables/variable.tf
@@ -6,3 +6,13 @@ variable "sample1" {
 output "sample1" {
   value = var.sample1
 }
+
+variable "fruits" {
+
+  default = ["apple", "banana", "mango"]
+
+}
+
+output "fruits" {
+  value = var.fruits[0]
+}
